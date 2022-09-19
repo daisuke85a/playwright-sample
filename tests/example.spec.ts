b2980214822e8832842f79634496345464efbd09
daisuke <dsaito85a@gmail.com>
2022-09-19 15:12:09 +0900

    feat: ghaでテスト動画をアップロードしてみる

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,21 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('homepage has Playwright in title and get started link linking to the intro page', async ({  }) => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext({ recordVideo: { dir: 'videos/' } });
+
+  // Open new page
+  const page = await context.newPage();
+
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
-  const getStarted = page.locator('text=Get Started failed');
+  const getStarted = page.locator('text=Get Started');
 
   // Expect an attribute "to be strictly equal" to the value.
   await expect(getStarted).toHaveAttribute('href', '/docs/intro');
@@ -17,4 +25,11 @@ test('homepage has Playwright in title and get started link linking to the intro
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
+
+  // Close page
+  await page.close();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
 });
